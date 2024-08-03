2a65b144414d14884e183fd1d09e604c1e2ba5d5
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-08-03 15:52:31 +0500

    Timeline Added

--- src/app/components/Details/Details.js
+++ src/app/components/Details/Details.js
@@ -1,16 +1,60 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./Details.module.css";
+import { motion } from "framer-motion";
+import "intersection-observer"; // Import the polyfill
 
 const Details = () => {
+  const [inView, setInView] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setInView(true);
+          observer.unobserve(ref.current); // Stop observing after animation has started
+        }
+      },
+      { threshold: 0.5 } // Adjust as needed
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("inView:", inView);
+  }, [inView]);
+
   return (
-    <div className="">
-      <div className={styles.gridContainer}>
+    <div>
+      <div ref={ref} className={styles.gridContainer}>
         <div className={styles.imageContainer}>
           <img
             className={styles.image}
             src="https://assets.website-files.com/600af88b128ed6334f4b7ef6/600b04a36ffbc054a6c046c6_Sign%20up.svg"
             alt="Sign up illustration"
           />
+        </div>
+        <div className="absolute top-1/2 left-1/2 translate-x-1/2 h-full">
+          <div className="absolute w-[30px] h-[30px] rounded-full -left-2 bg-[#006cd0] z-[50]"></div>
+          <span className="absolute w-[8px] h-full bg-gray-200 top-2 left-1/2 translate-x-1/2 z-[40]"></span>
+          {inView && (
+            <motion.span
+              initial={{ height: 0 }}
+              animate={{ height: "100%" }}
+              transition={{ duration: 2 }}
+              className="absolute w-[8px] h-full bg-[#006cd0] top-2 left-1/2 translate-x-1/2 z-[40]"
+            ></motion.span>
+          )}
         </div>
         <div className={styles.textContainer}>
           <p className={styles.stepNumber}>01</p>
@@ -31,6 +75,19 @@ const Details = () => {
             sint. Velit officia consequat duis enim velit mollit. Exercitation
             veniam consequat sunt nostrud amet.
           </p>
+        </div>
+        <div className="absolute top-1/2 left-1/2 translate-x-1/2 h-full">
+          <div className="absolute w-[30px] h-[30px] rounded-full -left-2 bg-[#006cd0] z-[50]"></div>
+          <span className="absolute w-[8px] h-full bg-gray-200 top-2 left-1/2 translate-x-1/2 z-[40]"></span>
+          {inView && (
+            <motion.span
+              initial={{ height: 0 }}
+              animate={{ height: "100%" }}
+              transition={{ duration: 2, delay: 2 }}
+              className="absolute w-[8px] h-full bg-[#006cd0] top-2 left-1/2 translate-x-1/2 z-[40]"
+            ></motion.span>
+          )}
+          <div className="absolute w-[30px] h-[30px] rounded-full -bottom-2 -left-[8px] bg-[#006cd0] z-[50]"></div>
         </div>
         <div className={styles.imageContainer}>
           <img
